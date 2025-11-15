78d9ffc7caaefed395579925bb1228011e7f2774
Kainoa-h <kainoahowj@gmail.com>
2025-11-15 15:02:46 +0800

    fix

--- Formula/kanata-layer-observer.rb
+++ Formula/kanata-layer-observer.rb
@@ -1,7 +1,7 @@
 class KanataLayerObserver < Formula
   desc "Simple tcp client that triggers a shell script when kanata switches layers"
   homepage "https://github.com/Kainoa-h/kanata-layer-observer"
-  url "https://github.com/Kainoa-h/kanata-layer-observer/releases/download/untagged-ca9036fbb01ce7c2b381/kanata-layer-observer-v1.0.0-macos-aarch64.tar.gz"
+  url "https://github.com/Kainoa-h/kanata-layer-observer/releases/download/v1.0.0/kanata-layer-observer-v1.0.0-macos-aarch64.tar.gz"
   version "1.0.0"
   sha256 "bcbceedb11665e50e7716845f3a38496057becafd5c35979012de3470267cc08"
   license "LGPL-3.0-or-later"
