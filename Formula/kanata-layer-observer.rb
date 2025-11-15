8fa1b8d182180c8941da489580ea31d4fb0015b1
Kainoa-h <kainoahowj@gmail.com>
2025-11-15 13:53:12 +0800

    add kanata-layer-observer

---
+++ Formula/kanata-layer-observer.rb
@@ -0,0 +1,23 @@
+class KanataLayerObserver < Formula
+  desc "Simple tcp client that triggers a shell script when kanata switches layers"
+  homepage "https://github.com/Kainoa-h/kanata-layer-observer"
+  url "https://github.com/Kainoa-h/kanata-layer-observer/releases/download/v1.0.0/kanata_layer_observer-v1.0.0-macos-aarch64.tar.gz"
+  version "1.0.0"
+  sha256 "7447b1e85d4fb8f130a994625a3eb29bc377cbfdeea329f862ad9169a268e329"
+  license "LGPL-3.0"
+
+  def install
+    bin.install "kanata_layer_observer"
+  end
+
+
+  service do
+    run opt_bin/"kanata-layer-observer"
+    keep_alive true
+    log_path var/"log/kanata-layer-observer.log"
+    error_log_path var/"log/kanata-layer-observer.err"
+    working_dir HOMEBREW_PREFIX
+    environment_variables PATH: std_service_path_env
+  end
+end
+
