ac6ef613253a3575cdd475ac7551336098c3cfef
Kainoa-h <kainoahowj@gmail.com>
2025-11-15 14:20:28 +0800

    fix kanata-layer-observer

--- Formula/kanata-layer-observer.rb
+++ Formula/kanata-layer-observer.rb
@@ -4,7 +4,10 @@ class KanataLayerObserver < Formula
   url "https://github.com/Kainoa-h/kanata-layer-observer/releases/download/v1.0.0/kanata_layer_observer-v1.0.0-macos-aarch64.tar.gz"
   version "1.0.0"
   sha256 "7447b1e85d4fb8f130a994625a3eb29bc377cbfdeea329f862ad9169a268e329"
-  license "LGPL-3.0"
+  license "LGPL-3.0-or-later"
+
+  # Only works on Apple Silicon Macs
+  depends_on arch: :arm64
 
   def install
     bin.install "kanata_layer_observer"
@@ -12,7 +15,7 @@ class KanataLayerObserver < Formula
 
 
   service do
-    run opt_bin/"kanata-layer-observer"
+    run opt_bin/"kanata_layer_observer"
     keep_alive true
     log_path var/"log/kanata-layer-observer.log"
     error_log_path var/"log/kanata-layer-observer.err"
